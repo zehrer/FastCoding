a978bc2caf245181981df4e2c259acac9f3f8ac0
Stephan Zehrer <stephan@zehrer.net>
2015-04-22 09:47:23 +0200

    updated (c)  -> FastCoding.h is a umbrella  header file for this framework setup

--- FastCoder/FastCoding.h
+++ FastCoder/FastCoding.h
@@ -1,9 +1,33 @@
 //
-//  FastCoderObjC.h
+//  FastCoding.h
 //
+//  Version 1.0
 //
-//  Created by Stephan Zehrer on 22.04.15.
+//  Created by Stephan Zehrer 04/22/2015
+//  Copyright (c) 2015 Stephan Zehrer
 //
+//  Distributed under the permissive zlib License
+//  Get the latest version from here:
+//
+//  https://github.com/zehrer/FastCoding
+//
+//  This software is provided 'as-is', without any express or implied
+//  warranty.  In no event will the authors be held liable for any damages
+//  arising from the use of this software.
+//
+//  Permission is granted to anyone to use this software for any purpose,
+//  including commercial applications, and to alter it and redistribute it
+//  freely, subject to the following restrictions:
+//
+//  1. The origin of this software must not be misrepresented; you must not
+//  claim that you wrote the original software. If you use this software
+//  in a product, an acknowledgment in the product documentation would be
+//  appreciated but is not required.
+//
+//  2. Altered source versions must be plainly marked as such, and must not be
+//  misrepresented as being the original software.
+//
+//  3. This notice may not be removed or altered from any source distribution.
 //
 
 #import <Cocoa/Cocoa.h>
@@ -14,6 +38,6 @@ FOUNDATION_EXPORT double FastCoderVersionNumber;
 //! Project version string for FastCoderObjC.
 FOUNDATION_EXPORT const unsigned char FastCoderVersionString[];
 
-// In this header, you should import all the public headers of your framework using statements like #import <FastCoderObjC/PublicHeader.h>
+// In this header, you should import all the public headers of your framework using statements like #import <FastCoder/PublicHeader.h>
 
-
+// This umbrella header file is used to compile the both versions as a framework. 
