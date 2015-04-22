78f9c08df31d2c0455710b1fd75228566b85c786
Stephan Zehrer <stephan@zehrer.net>
2015-04-22 09:22:41 +0200

    created a new project for migration to SWIFT

---
+++ FastCoder/FastCoding.h
@@ -0,0 +1,19 @@
+//
+//  FastCoderObjC.h
+//
+//
+//  Created by Stephan Zehrer on 22.04.15.
+//
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for FastCoderObjC.
+FOUNDATION_EXPORT double FastCoderVersionNumber;
+
+//! Project version string for FastCoderObjC.
+FOUNDATION_EXPORT const unsigned char FastCoderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FastCoderObjC/PublicHeader.h>
+
+
